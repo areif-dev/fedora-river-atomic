b93bd6905069aa7c8c88fc616db8a8f7c5e4ced7
areif-dev <ajreifsnyder@protonmail.com>
2024-06-05 15:36:55 -0400

    Remove arch-gaming for testing purposes

--- distroboxes/arch-gaming/installpkgs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-
-packages=$(cat /home/aur-builder/aur-packages.txt)
-for package in $packages; do 
-    pacman -U --noconfirm /home/aur-builder/$package/$package-*.pkg.tar*
-done
--- distroboxes/arch-gaming/makepkgs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-
-packages=$(cat /home/aur-builder/aur-packages.txt)
-for package in $packages; do 
-    git clone https://aur.archlinux.org/$package.git /home/aur-builder/$package
-    cd /home/aur-builder/$package 
-    makepkg
-done
