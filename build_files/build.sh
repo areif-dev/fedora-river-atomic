41f20b70e462421a3fab37c312ba9c619d2e595e
areif-dev <aj@ajreifsnyder.com>
2025-07-18 14:00:01 -0400

    Add build scripts

---
+++ build_files/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+set -ouex pipefail 
+
+# Speed up DNF
+sed -i 's/.*max_parallel_downloads=.*//g' /etc/dnf/dnf.conf && \
+sed -i 's/.*fastestmirror=.*//g' /etc/dnf/dnf.conf && \
+echo 'max_parallel_downloads=15' | tee -a /etc/dnf/dnf.conf && \
+echo 'fastestmirror=True' | tee -a /etc/dnf/dnf.conf
+
+# Install Extra Repos
+dnf5 install -y \
+https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && \
+curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo 
+
+source /ctx/packages.sh
+echo "${all_packages[@]}"
